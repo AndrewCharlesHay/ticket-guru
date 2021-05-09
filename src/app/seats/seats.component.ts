2fd95070375e21712aad2d8e5f10c2757d1f07a2
Andrew Hay <andrewcharleshay96@gmail.com>
2021-05-09 14:13:32 -0400

    Made Seats table

---
+++ src/app/seats/seats.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-seats',
+  templateUrl: './seats.component.html',
+  styleUrls: ['./seats.component.css']
+})
+export class SeatsComponent implements OnInit {
+  @Input() performanceId: number;
+  displayedColumns = ['name', 'price', 'available'];
+
+  seats: any = {}
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit(): void {
+    this.dataService.getSeatsByPerformance(2).subscribe(seat => this.seats = seat)
+  }
+
+}
